2454c484511d0790664f0a15dc5b41d73337710e
AbdAlftahSalem <abd321alftah@gmail.com>
2023-07-23 12:53:46 +0300

    Finish categories

--- src/app/rout_index.js
+++ src/app/rout_index.js
@@ -1,8 +1,9 @@
 const AuthRout = require("../app/auth/auth_rout");
-
+const CategoryRout = require("../app/category/category_rout");
 
 const mountRoutes = (app) => {
     app.use('/api/v1/auth', AuthRout)
+    app.use('/api/v1/categories', CategoryRout)
 };
 
 module.exports = mountRoutes;
